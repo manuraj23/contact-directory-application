78873d4ab8a7deaa6d803f7b7a241f77df448fa7
het-naik <141618536+het-naik@users.noreply.github.com>
2026-02-03 21:07:12 +0530

    Contact App Task - 1

--- add-contact.ts
+++ add-contact.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { JsonPipe } from '@angular/common';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { Contact } from '../view-contact/contactinterface';
-
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [ReactiveFormsModule, JsonPipe, MatInputModule, MatButtonModule, MatSelectModule],
+  imports: [CommonModule, ReactiveFormsModule, JsonPipe, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './add-contact.html',
   styleUrl: './add-contact.css',
 })
 export class AddContact implements OnInit {
 
-  contactForm! : FormGroup;
+  countryCodes: {
+    name: string;
+    dial_code: string;
+    code: string;
+  }[] = [];
 
-  constructor(private fb : FormBuilder) {}
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private http : HttpClient) { }
 
   ngOnInit() {
     this.contactForm = this.fb.group({
@@ -25,14 +32,22 @@ export class AddContact implements OnInit {
       countryCode: ['', [Validators.required]],
       contactNumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
     });
+
+    this.loadCountryCodes();
+  }
+
+  loadCountryCodes() {
+    this.http.get<any[]>('https://gist.githubusercontent.com/Goles/3196253/raw/CountryCodes.json').subscribe((data) => {
+      this.countryCodes = data.sort((a,b) => a.name.localeCompare(b.name));
+    })
   }
 
   AddContact() {
-    if(this.contactForm.invalid) {
+    if (this.contactForm.invalid) {
       return;
     }
 
-    const newContact : Contact = this.contactForm.value;
+    const newContact: Contact = this.contactForm.value;
 
     const storedContacts = localStorage.getItem('contacts');
     const contacts = storedContacts ? JSON.parse(storedContacts) : [];
